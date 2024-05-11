83e79508e16ad5e4408eaa27fbbd6165ff61158c
kpauner <kpauner@gmail.com>
2024-05-11 13:29:21 +0200

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
+import { Button } from '@/components/ui/button'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8787/api/posts')
+      .then(response => {
+        // Check if the response is ok (status code 200-299)
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPosts(data); 
+      
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []); 
+
 
   return (
     <>
@@ -15,19 +36,14 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <div>
+      <h1>Posts</h1>
+      <pre><code>{JSON.stringify(posts, null, 2)}</code></pre>
+ 
+    </div>
+        <Button variant="outline">Outline</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
