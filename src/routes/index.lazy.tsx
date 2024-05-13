0c4a48e59ae4d38f37c4b5dca3092fa1cabf447c
kpauner <kpauner@gmail.com>
2024-05-13 17:26:43 +0200

    feat: gallery

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,10 +1,10 @@
+import Gallery from "@/components/gallery";
 import Heading from "@/components/layout/heading";
 import PageTemplate from "@/components/layout/page-template";
 import SectionTemplate from "@/components/layout/section-template";
 import Hero from "@/components/marketing/hero";
 import { Separator } from "@/components/ui/separator";
-import { fetchPosts } from "@/queries";
-import { useQuery } from "@tanstack/react-query";
+import { useProjects } from "@/queries";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -12,24 +12,38 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Index() {
-  const query = useQuery({ queryKey: ["posts"], queryFn: fetchPosts });
+  const { data, error, status } = useProjects();
+
+  console.log(data);
   return (
     <PageTemplate>
       <Hero />
-      <Separator className="bg-white/30" />
-      <div className="flex items-center justify-between gap-4">
-        <Heading
-          as="h2"
-          size="xs"
-          className="font-neue font-thin uppercase tracking-widest text-white"
-        >
-          About
-        </Heading>
-        <span>01</span>
-      </div>
-      <SectionTemplate className="bg-amber-400/30">
+      <SectionTemplate>
+        <div className="space-y-4 py-12">
+          <Separator className="bg-white/30" />
+          <div className="flex items-center justify-between gap-4">
+            <Heading
+              as="h2"
+              size="xs"
+              className="font-neue font-thin uppercase tracking-widest text-white"
+            >
+              Featured Projects
+            </Heading>
+            <span>01</span>
+          </div>
+        </div>
+        {status === "pending" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <Gallery data={data} />
+          </>
+        )}
+
         <pre>
-          <code>{JSON.stringify(query, null, 2)}</code>
+          <code>{JSON.stringify(data?.items, null, 2)}</code>
         </pre>
       </SectionTemplate>
     </PageTemplate>
