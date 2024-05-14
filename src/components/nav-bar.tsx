1e2527ac846bea1b13a3a7715c5e57e139a7d928
kpauner <kpauner@gmail.com>
2024-05-14 11:48:00 +0200

    feat: animated menu

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,6 +1,8 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Icons } from "./icons";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { Link } from "@tanstack/react-router";
+import { useRef } from "react";
 import { NavItem } from "@/types";
 
 type NavBarProps = {
@@ -8,27 +10,73 @@ type NavBarProps = {
 };
 
 export default function NavBar({ items }: NavBarProps) {
+  const container = useRef<HTMLDivElement>(null);
+  const tl = useRef();
+
+  const { contextSafe } = useGSAP(
+    () => {
+      tl.current = gsap
+        .timeline()
+        .to(".nav", {
+          y: "-30px",
+          autoAlpha: 0,
+          ease: "power2.inOut",
+        })
+        .to(".bar", {
+          height: "0",
+          delay: 0.2,
+          duration: 0.4,
+          stagger: {
+            amount: 1,
+            grid: [2, 1],
+            axis: "y",
+            ease: "circk.inOut",
+            from: "start",
+          },
+          ease: "power2.inOut",
+        });
+    },
+    { scope: container },
+  );
+
+  const toggleTimeline = contextSafe(() => {
+    tl.current.reversed(!tl.current.reversed());
+  });
+
   return (
-    <div>
-      <div className="fixed left-0 right-0 top-0 z-10 flex justify-between gap-2 p-6">
+    <div className="fixed left-0 right-0 top-0 z-20 w-full ">
+      <div className="flex w-full max-w-screen-2xl justify-between gap-2  p-6">
         <Icons.logo className="h-6 fill-white" />
         <div className="flex items-center gap-2">
-          <Sheet>
-            <SheetTrigger>Open</SheetTrigger>
-            <SheetContent side="top">
-              <nav className="flex items-center gap-2">
+          <div ref={container} className="h-0">
+            <button
+              className="fixed right-6 top-6 z-50 text-white"
+              onClick={toggleTimeline}
+            >
+              Toggle
+            </button>
+            <div className="fixed left-0 top-0 z-40 flex">
+              {Array.from({ length: 5 }, (_, index) => (
+                <span
+                  key={index}
+                  className="bar block h-screen w-[20vw] bg-black"
+                ></span>
+              ))}
+
+              <nav className="nav fixed left-0 top-0 z-40 flex h-full w-full flex-col items-center justify-center text-white opacity-100">
                 {items.map((item) => (
                   <Link
                     key={item.name}
                     to={item.href}
-                    className="[&.active]:font-bold"
+                    onClick={toggleTimeline}
+                    className=" [&.active]:font-bold"
                   >
                     {item.name}
                   </Link>
                 ))}
               </nav>
-            </SheetContent>
-          </Sheet>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/providers/modal-provider.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export default function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return <>{/* PLACE GLOBAL DIALOGS HERE */}</>;
+}
