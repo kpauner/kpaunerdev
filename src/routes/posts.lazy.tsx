e11c81ce3697627a2ecf1e16ad2424783574a23c
kpauner <kpauner@gmail.com>
2024-06-19 19:01:22 +0200

    initial commit

--- src/routes/posts.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/posts")({
-  component: Posts,
-});
-
-function Posts() {
-  return <div className="p-2">Hello from Posts!</div>;
-}
