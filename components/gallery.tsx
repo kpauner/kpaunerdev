6ed5abd9df4a198e76ae10c37b4439569b67b316
kpauner <kpauner@gmail.com>
2024-07-11 16:22:55 +0200

    fix: gallery hover

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -11,6 +11,7 @@ type GalleryProps = {
 
 export default function Gallery({ locale }: GalleryProps) {
   const { data, error, isLoading } = useProjects();
+
   console.log(data);
 
   //TODO PLACE VIDEO EXTENSIONS IN A CONFIG FILE
@@ -21,42 +22,34 @@ export default function Gallery({ locale }: GalleryProps) {
   };
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {data?.items?.map((item) => (
         <div
           key={item.id}
-          className="relative group hover:cursor-pointer rounded-xl overflow-hidden  border border-white/10"
+          className="relative overflow-hidden group aspect-w-16 aspect-h-9"
         >
-          {isVideo(item.featured_img) ? (
-            <video
-              src={`${process.env.NEXT_PUBLIC_API_URL}/api/files/${item.collectionName}/${item.id}/${item.featured_img}`}
-              width="1000"
-              height="1000"
-              className="w-full h-full object-cover"
-              autoPlay
-              loop
-              muted
-              playsInline
-            />
-          ) : (
-            <Image
-              src={`${process.env.NEXT_PUBLIC_API_URL}/api/files/${item.collectionName}/${item.id}/${item.featured_img}`}
-              width={1000}
-              height={1000}
-              alt={item.title}
-              className="w-full h-full object-cover"
-            />
-          )}
-          <div className="text-sm w-full text-white bg-black/80 absolute top-0 right-0 p-4 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 ">
-            <div className="border-b border-dotted border-white py-2 flex justify-between items-center mb-4">
-              <h3 className="font-medium uppcase text-xs uppercase tracking-wider">
-                {item.title}
-              </h3>
-              <Icons.arrowupright className="w-4 h-4 fill-white" />
-            </div>
-            <p className="">
-              {item[`description_${locale}` as keyof typeof item]}
-            </p>
+          <div className="transition-transform duration-300 group-hover:scale-110 w-full h-full">
+            {isVideo(item.featured_img) ? (
+              <video
+                src={`${process.env.NEXT_PUBLIC_API_URL}/api/files/${item.collectionName}/${item.id}/${item.featured_img}`}
+                className="w-full h-full object-cover"
+                muted
+                loop
+                autoPlay
+                playsInline
+              />
+            ) : (
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_URL}/api/files/${item.collectionName}/${item.id}/${item.featured_img}`}
+                alt={item.title}
+                className="object-cover w-full h-full"
+                width={1000}
+                height={1000}
+              />
+            )}
+          </div>
+          <div className="absolute inset-0 bg-black/50 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+            <h3 className="text-white text-xl font-bold">{item.title}</h3>
           </div>
         </div>
       ))}
